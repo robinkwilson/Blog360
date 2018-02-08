4dff1fde1c9bb042872ecdef4492c9249cac80d0
robin <robin.kath.wilson@gmail.com>
2018-02-08 15:34:27 -0800

    added redux dependencies and dummy db data

---
+++ components/content/dummydata.js
@@ -0,0 +1,102 @@
+// dummy data simulating NoSQL database schema
+
+const pages = [
+  {
+    name: 'North America',
+    id: 1,
+    blog_title: 'Welcome to Blog360',
+    blog_text: 'Filler text.',
+    isHome: true,
+    left_icons: [],
+    right_icons: []
+  },
+  {
+    name: 'Eastern Europe',
+    id: 2,
+    blog_title: 'Eastern Europe',
+    blog_text: 'Filler text.',
+    isHome: false,
+    left_icons: [],
+    right_icons: []
+  },
+  {
+    name: 'Germany',
+    id: 3,
+    blog_title: 'Germany',
+    blog_text: 'Filler text.',
+    isHome: false,
+    left_icons: [],
+    right_icons: []
+  },
+  {
+    name: 'California',
+    id: 4,
+    blog_title: 'Oakland',
+    blog_text: 'Filler text.',
+    isHome: false,
+    left_icons: [],
+    right_icons: []
+  },
+  {
+    name: 'Washington',
+    id: 5,
+    blog_title: 'Seattle',
+    blog_text: 'Filler text.',
+    isHome: false,
+    left_icons: [],
+    right_icons: []
+  }
+];
+
+const left_icons = [
+  {
+    id: 1,
+    thumbnail_img: '',
+    pano_id: -1,
+    location: {lat: 20, long: -20},
+    order: 1,
+  },
+  {
+    id: 2,
+    thumbnail_img: '',
+    pano_id: -1,
+    location: {lat: 20, long: -20},
+    order: 1,
+  },
+  {
+    id: 2,
+    thumbnail_img: '',
+    pano_id: -1,
+    location: {lat: 20, long: -20},
+    order: 1,
+  },
+  {
+    id: 2,
+    thumbnail_img: '',
+    pano_id: -1,
+    location: {lat: 20, long: -20},
+    order: 1,
+  }
+];
+
+const right_icons = [
+  {
+    id: 1,
+    thumbnail_img: '',
+    pano_id: 1,
+    title: 'xxx',
+    date: 'xxx',
+    page_id: 1,
+    order: 1
+  }
+]
+
+const panos = [
+  {
+    id: 1,
+    title: 'Hello',
+    location: {lat: 20, long: -20},
+    date: 'xxx',
+    order: 1
+  }
+]
