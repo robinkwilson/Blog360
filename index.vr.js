22d2d1a8f7f6fb8271aea0a71012032c68fdd079
robin <robin.kath.wilson@gmail.com>
2018-02-09 10:57:33 -0800

    added Home2 to render using redux store

--- index.vr.js
+++ index.vr.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { AppRegistry } from 'react-vr';
 import Home from './components/Home.js';
+import Home2 from './components/Home2.js';
+import { Provider } from 'react-redux';
+import store from './store';
 
 export default class Blog360 extends React.Component {
   render() {
+    // return (
+    //   <Provider store={store}>
+    //     <Home2 />
+    //   </Provider>
+    // );
     return (
       <Home />
-    )
+    );
   }
 }
 
